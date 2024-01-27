87aaaf8ed0698c3a34925deb3f52e2766aab1d09
Aleksandr <cross_comp@hotmail.com>
2024-01-28 01:07:13 +0300

    add HW: task1

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте двумерный массив символов (тип char [,]).
+// Создать строку из символов этого массива.
+
+string ArrCharsToString(char[,] array)
+{
+    //int size = array.GetLength(0) * array.GetLength(1);
+    string str = "";
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            str += array[i, j];
+        }
+    }
+    return str;
+}
+char[,] array_chars = {
+    {'H','e','l','l','o',' '},
+    {'c','r','a','z','y',' '},
+    {'W', 'o', 'r', 'l','d','!'}
+};
+Console.WriteLine(ArrCharsToString(array_chars));
+
+
+
